d8a02befa08082c4cea586466ea39e9dfe379de9
seongjju <jeongsj1226@naver.com>
2024-05-11 19:06:57 +0900

    test7

--- src/main/java/entity/People.java
+++ src/main/java/entity/People.java
@@ -9,7 +9,7 @@ public class People {
         System.out.println("test4");
         System.out.println("test5");
         System.out.println("test6");
-
+        System.out.println("test7");
 
 
     }
